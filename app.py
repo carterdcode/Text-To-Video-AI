1e6a8e6d0c1e1eeb8919aa6f954e2dd4a05968c3
carterdcode <117289619+carterdcode@users.noreply.github.com>
2024-12-16 02:21:59 +0000

    testing gpt4free

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import edge_tts
 import json
 import asyncio
 import whisper_timestamped as whisper
-from utility.script.script_generator import generate_script
+from utility.gpt4free import generate_script_free
 from utility.audio.audio_generator import generate_audio
 from utility.captions.timed_captions_generator import generate_timed_captions
 from utility.video.background_video_generator import generate_video_url
@@ -17,6 +17,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a video from a template and topic. The templates are: facts, mens, travel")
     parser.add_argument("--template", type=str, help="The template for the video", default="facts")
     parser.add_argument("topic", type=str, help="The specific topic for the video")
+    # to run the script, use the following command:
+    # python app.py --template facts "The topic of the video"
 
     args = parser.parse_args()
     SAMPLE_TEMPLATE = args.template
@@ -24,7 +26,7 @@ if __name__ == "__main__":
     SAMPLE_FILE_NAME = "audio_tts.wav"
     VIDEO_SERVER = "pexel"
 
-    response = generate_script(SAMPLE_TEMPLATE, SAMPLE_TOPIC)
+    response = generate_script_free(SAMPLE_TEMPLATE, SAMPLE_TOPIC)
     print("script: {}".format(response))
 
     asyncio.run(generate_audio(response, SAMPLE_FILE_NAME))
